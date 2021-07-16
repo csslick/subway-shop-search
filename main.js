f3bdfb14abbaa9212ddedfbba04a223feb650cef
csslick <digitone@naver.com>
2021-07-17 00:22:02 +0900

    카카오맵과 검색

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const shops = document.querySelector('.shops');
+let html = `
+  <tr>
+    <th>NO</th>
+    <th>매장명</th>
+    <th>매장주소</th>
+    <th>주요서비스</th>
+    <th>연락처</th>
+  </tr>  
+`
 
-shopDB.forEach(function(shop) {
-  console.log(shop.title)
+// 매장 출력(내림차순)
+shopDB.reverse().forEach(function(shop) {
+  // console.log(shop.title)
   showShops(shop);
 })
+
+// 총 *개 매장이 있습니다.
+const shop_count = document.querySelector('.board_total > .shop_count');
+shop_count.innerText = shopDB.length;
 
 function showShops(shop) {
   const id = shop.id;
@@ -11,16 +25,19 @@ function showShops(shop) {
   const address = shop.address;
   const service = shop.service;
   const tel = shop.tel;
-  console.log(service)
-  const html = `
+  // console.log(service)
+  html += `
       <tr>
         <td>${id}</td>
-        <td>${title}</td>
-        <td>${address}</td>
-        <td>${service}</td>
+        <td><a href='#'>${title}</a></td>
+        <td class='address'><a href="#">${address}</a></td>
+        <td class='service'>
+          <span ${service.morning ? "class='accent'": ""}>아침메뉴</span>
+          <span ${service.roundTheClock ? "class='accent'": ""}>24시간</span>
+        </td>
         <td>${tel}</td>
       </tr>  
     `
-  shops.innerHTML += html;  
+  shops.innerHTML = html;  
 }
 
--- shopDB.js
+++ shopDB.js
@@ -1,17 +1,17 @@
 // 매장 DB
 const shopDB = [
   {
-    id: 454,
+    id: 1,
     title: '중계은행사거리',
     address: '서울특별시 노원구 한글비석로 269',
     service: {
       morning: true,
-      roundTheClock: true,
+      roundTheClock: false,
     },
     tel: '02-952-2266',
   },
   {
-    id: 453,
+    id: 2,
     title: '테헤란로',
     address: '서울특별시 강남구 테헤란로 314 지하1층',
     service: {
@@ -21,7 +21,7 @@ const shopDB = [
     tel: '02-555-1014',
   },
   {
-    id: 452,
+    id: 3,
     title: '부산롯데백화점광복',
     address: '부산시 중구 중앙대로2 4층',
     service: {
@@ -31,7 +31,7 @@ const shopDB = [
     tel: 'Comming Soon',
   },
   {
-    id: 451,
+    id: 4,
     title: '대구율하롯데몰',
     address: '대구광역시 동구 안심로 80, 1층(율하동, 롯데쇼핑프라자)',
     service: {
@@ -41,7 +41,7 @@ const shopDB = [
     tel: '053-607-2780',
   },
   {
-    id: 450,
+    id: 5,
     title: '광주신용',
     address: '광주 북구 첨단연신로 91번길 10',
     service: {
@@ -51,7 +51,7 @@ const shopDB = [
     tel: '062-575-0614',
   },
   {
-    id: 449,
+    id: 6,
     title: '상봉역',
     address: '서울특별시 중랑구 망우로 316',
     service: {
@@ -59,5 +59,45 @@ const shopDB = [
       roundTheClock: false,
     },
     tel: '02-432-2938',
+  },
+  {
+    id: 7,
+    title: '부산하단아트몰링',
+    address: '부산시 사하구 낙동남로1413',
+    service: {
+      morning: true,
+      roundTheClock: false,
+    },
+    tel: '051-999-7301',
+  },
+  {
+    id: 8,
+    title: '광주신창',
+    address: '광주광역시 광산구 신창로 121 1층',
+    service: {
+      morning: false,
+      roundTheClock: false,
+    },
+    tel: '062-953-1131',
+  },
+  {
+    id: 9,
+    title: '고양원흥역',
+    address: '경기도 고양시 덕양구 원흥5로 25',
+    service: {
+      morning: false,
+      roundTheClock: false,
+    },
+    tel: '031-965-9965',
+  },
+  {
+    id: 10,
+    title: '신정네거리역',
+    address: '서울특별시 양천구 중앙로 265',
+    service: {
+      morning: true,
+      roundTheClock: false,
+    },
+    tel: '02-2607-9979',
   },
 ]
